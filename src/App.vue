aae2bd64b5efd6f6fdd7f80ebb969910106eb16a
AvaniSujith <chinchuavani123@gmail.com>
2025-06-21 15:24:43 +0530

    feat: learn state

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const message = ref('hello world')
+</script>
+
 <template>
   <main>
-    <h2>Hello world</h2>
+    <h2>{{ message }}</h2>
   </main>
 </template>
 
 <style scoped>
-
+main{
+  max-width:  800px;
+  margin: 1rem auto;
+}
 </style>
