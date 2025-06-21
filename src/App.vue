a5769b61cccc74767aad5c8412d04f247d9aa30b
AvaniSujith <chinchuavani123@gmail.com>
2025-06-21 15:35:33 +0530

    feat: input bar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script lang="ts" setup>
 import { ref } from "vue";
 
-const message = ref('hello world')
+const message = ref('hello world');
+const newTask = ref("");
 </script>
 
 <template>
   <main>
     <h2>{{ message }}</h2>
+    <input name="newTask" v-model="newTask" >
+    <p>{{ newTask }}</p>
   </main>
 </template>
 
